cf4027b8cb3eb9a2aed170d2bb686c4cc5f4e38e
luizjunior8 <luiz.oliveira@dcx.ufpb.br>
2020-09-21 01:38:22 -0300

    Mission Accomplished

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -15,8 +15,73 @@ public class CupomFiscal {
 	public static String CNPJ = "42.591.651/0797-34";
 	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
 
+	public static String Spacer = System.lineSeparator();
+
+	public static void typerwriter(){
+		
+		if (NOME_LOJA.isEmpty()){
+			System.out.printf("O campo nome da loja é obrigatório");
+			return;
+		}
+
+		if (LOGRADOURO.isEmpty()){
+			System.out.printf("O campo logradouro do endereço é obrigatório");
+			return;
+		}
+
+		String _logradouro = LOGRADOURO + ", ";
+		String _numero = "0";
+
+		if (NUMERO == 0){
+			_numero = "s/n";
+		}else{
+			_numero = String.format ("%d", NUMERO);
+		}
+				
+		String _complemento = " " + COMPLEMENTO;
+		String _bairro = BAIRRO.isEmpty()? "" : BAIRRO + " - ";
+
+		if (MUNICIPIO.isEmpty()){
+			System.out.printf("O campo município do endereço é obrigatório");
+			return;
+		}
+
+		String _municipio = MUNICIPIO + " - ";
+
+		if (ESTADO.isEmpty()){
+			System.out.printf("O campo estado do endereço é obrigatório");
+			return;
+		}
+
+		String _cep = CEP.isEmpty()? "" : "CEP:" + CEP;
+		String _telefone = TELEFONE.isEmpty()? "" : " Tel " + TELEFONE;
+
+		if (CNPJ.isEmpty()){
+			System.out.printf("O campo CNPJ da loja é obrigatório");
+			return;
+		}
+
+		String _cnpj = "CNPJ: " + CNPJ;
+
+		if (INSCRICAO_ESTADUAL.isEmpty()){
+			System.out.printf("O campo inscrição estadual da loja é obrigatório");
+			return;
+		}
+
+		String _inscricao_estadual = "IE: " + INSCRICAO_ESTADUAL;
+
+		System.out.println (NOME_LOJA + Spacer +
+		_logradouro + _numero + _complemento + Spacer +
+		_bairro + _municipio + ESTADO + Spacer +
+		_cep + _telefone + Spacer +
+		OBSERVACAO + Spacer +
+		_cnpj + Spacer +
+		_inscricao_estadual + Spacer);	
+	}
+
 	public static void main(final String[] args) {
-		//Implemente aqui
+		typerwriter();		
 	}
 
 }
+ 
